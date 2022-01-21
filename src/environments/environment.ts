631aa77e731869213073d74dbca448621d51b2fd
RubenW556 <85158519+RubenW556@users.noreply.github.com>
2022-01-21 17:15:49 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   serverURL: "http://20.123.150.56:8080"
 };
 
